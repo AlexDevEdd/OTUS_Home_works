a6ccd98cba577ce7dfcba2ed67459e279c2c1254
AlexDevEdd <92832034+AlexDevEdd@users.noreply.github.com>
2023-12-10 08:11:33 +0300

    Init project

---
+++ Assets/_Project/Scripts/Core/Installers/ScriptableObjects/GameBalanceInstaller.cs
@@ -0,0 +1,17 @@
+using _Project.Scripts.Core.Installers.ScriptableObjects;
+using UnityEngine;
+using Zenject;
+
+namespace _Game.Scripts.Core.Installers.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "GameBalanceInstaller", menuName = "Installers/GameBalanceInstaller")]
+    public class GameBalanceInstaller : ScriptableObjectInstaller<GameBalanceInstaller>
+    {
+        [SerializeField] private GameBalance _balance;
+        public override void InstallBindings()
+        {
+            Container.BindInterfacesAndSelfTo<GameBalance>().FromInstance(_balance).AsSingle();
+
+        }
+    }
+}
